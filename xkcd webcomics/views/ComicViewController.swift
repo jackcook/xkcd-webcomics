76b81732c6771793f7500c4d699c1e3cc86f02cf
Jack Cook <jack@jackcook.us>
2014-11-29 22:57:57 -0500

    Finished layout for ComicViewController

---
+++ xkcd webcomics/views/ComicViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ComicViewController.swift
+//  xkcd webcomics
+//
+//  Created by Jack Cook on 11/29/14.
+//  Copyright (c) 2014 CosmicByte. All rights reserved.
+//
+
+import UIKit
+
+class ComicViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func preferredStatusBarStyle() -> UIStatusBarStyle {
+        return UIStatusBarStyle.LightContent
+    }
+}
